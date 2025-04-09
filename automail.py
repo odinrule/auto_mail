136b316b7321124f0db5c5c97e43143441bf3882
Richie <yc.richie@gmail.com>
2025-04-09 16:33:34 +0800

    starting auto email repository

---
+++ automail.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+from datetime import date
+
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Sender Name'
+email['to'] = 'receiver email address'
+email['subject'] = 'You won the twillio! And html file template.'
+
+today = date.today()
+# email.set_content(html.substitute({'name': 'TinTin'}), 'html')  # using dictionary
+email.set_content(html.substitute(name='TinTin', date=today),
+                  'html')   # using keyword arguement
+
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('sender email address', 'sender email password')
+    smtp.send_message(email)
+    print('all good boss!')
