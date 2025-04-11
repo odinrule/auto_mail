04f88b453f6869cf34a7fc5934a52d740fd757f1
Richie <yc.richie@gmail.com>
2025-04-11 16:59:37 +0800

    add some comment about personal data in the program

--- automail.py
+++ automail.py
@@ -7,8 +7,9 @@ from datetime import date
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'Sender Name'
-email['to'] = 'receiver email address'
+email['from'] = 'Sender Name'  # add your Sender Name
+email['to'] = 'receiver email address'  # add your receiver email address
+# add your subject
 email['subject'] = 'You won the twillio! And html file template.'
 
 today = date.today()
@@ -21,6 +22,7 @@ with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
     smtp.login('sender email address', 'sender email password')
+    # 'sender email address', 'sender email password' is added by user
     # if it is gmail account, you should do the 2 step verification and get the application password.
     smtp.send_message(email)
     print('Email has been sent!')
